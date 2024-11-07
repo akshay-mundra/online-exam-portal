cc7074450c60503680c4d4afbbfe446bcdcb5a24
Akshay Mundra <akshay@gkmit.co>
2024-11-07 11:32:08 +0530

    feat: [oep-2] husky pre-commit setup

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 const startServer = async function () {
-	try {
-		app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-	} catch (err) {
-		console.log('Error runing server', err);
-	}
+  try {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  } catch (err) {
+    console.log('Error runing server', err);
+  }
 };
 
 startServer();
