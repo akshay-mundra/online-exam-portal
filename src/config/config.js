dd153141edb1754387a6f77f0fc2bbd31669b44a
Akshay Mundra <akshay@gkmit.co>
2024-11-07 12:46:19 +0530

    fix: [oep-4] indentations in config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,36 +1,36 @@
 require('dotenv').config({ path: __dirname + '/../../.env' });
 
 module.exports = {
-	development: {
-		host: process.env.DB_HOST,
-		dialect: process.env.DB_DIALECT,
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_DATABASE,
-		logging: process.env.DB_LOGGING === 'true' ? console.log : false,
-		define: {
-			paranoid: true,
-			underscored: true,
-			timestamps: true,
-			createdAt: 'created_at',
-			updatedAt: 'updated_at',
-			deletedAt: 'deleted_at',
-		},
-	},
-	test: {
-		host: '',
-		dialect: '',
-		username: '',
-		password: '',
-		database: '',
-		logging: process.env.DB_LOGGING === 'true' ? console.log : false,
-		define: {
-			paranoid: true,
-			underscored: true,
-			timestamps: true,
-			createdAt: 'created_at',
-			updatedAt: 'updated_at',
-			deletedAt: 'deleted_at',
-		},
-	},
+  development: {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    logging: process.env.DB_LOGGING === 'true' ? console.log : false,
+    define: {
+      paranoid: true,
+      underscored: true,
+      timestamps: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+      deletedAt: 'deleted_at',
+    },
+  },
+  test: {
+    host: '',
+    dialect: '',
+    username: '',
+    password: '',
+    database: '',
+    logging: process.env.DB_LOGGING === 'true' ? console.log : false,
+    define: {
+      paranoid: true,
+      underscored: true,
+      timestamps: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+      deletedAt: 'deleted_at',
+    },
+  },
 };
