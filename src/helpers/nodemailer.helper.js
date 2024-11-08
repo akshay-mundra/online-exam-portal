5605261de608eae62953fc30e68e58b578e6db82
Akshay Mundra <akshay@gkmit.co>
2024-11-08 11:10:58 +0530

    feat: [oep-6] nodemailer setup

---
+++ src/helpers/nodemailer.helper.js
@@ -0,0 +1,24 @@
+const nodeMailer = require('nodemailer');
+
+async function sendEmail(options) {
+  const transporter = nodeMailer.createTransport({
+    host: process.env.SMPT_HOST,
+    port: process.env.SMPT_PORT,
+    auth: {
+      user: process.env.SMPT_MAIL,
+      pass: process.env.SMPT_APP_PASS,
+    },
+    authMethod: 'LOGIN',
+  });
+
+  const mailOptions = {
+    from: process.env.SMPT_MAIL,
+    to: options.to,
+    subject: options.subject,
+    html: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+}
+
+module.exports = { sendEmail };
