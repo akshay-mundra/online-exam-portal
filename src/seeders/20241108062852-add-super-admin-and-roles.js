00c15b7a8e69c9ba8e0f0e9f9b023ddf4b6e731a
Akshay Mundra <akshay@gkmit.co>
2024-11-08 14:40:58 +0530

    feat: [oep-7] add seed for superadmin and roles

---
+++ src/seeders/20241108062852-add-super-admin-and-roles.js
@@ -0,0 +1,55 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    const [roles] = await queryInterface.bulkInsert(
+      'roles',
+      [
+        {
+          name: 'super_admin',
+        },
+        {
+          name: 'admin',
+        },
+        {
+          name: 'user',
+        },
+      ],
+      { returning: ['id'] },
+    );
+
+    const [super_admin] = await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          first_name: 'Akshay',
+          last_name: 'Mundra',
+          email: 'akshay.mundra1010@gmail.com',
+          password: await bcrypt.hash(process.env.SUPER_ADMIN_PASS, 10),
+        },
+      ],
+      { returning: ['id'] },
+    );
+
+    let super_admin_role_id = roles.id;
+    const admin_id = super_admin.id;
+
+    await queryInterface.bulkInsert('users_roles', [
+      {
+        user_id: admin_id,
+        role_id: super_admin_role_id,
+      },
+    ]);
+  },
+
+  async down() {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
