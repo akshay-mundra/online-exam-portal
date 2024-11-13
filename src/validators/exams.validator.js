fde3e35b92f4b2191738db2eaa98f65fa44fa30d
Akshay Mundra <akshay@gkmit.co>
2024-11-14 00:01:42 +0530

    feat: [oep-21] add exam validators

--- src/validators/exams.validator.js
+++ src/validators/exams.validator.js
@@ -2,7 +2,22 @@ const Joi = require('joi');
 const validateHelpers = require('../helpers/validators.helper');
 const commonHelpers = require('../helpers/common.helper');
 
-async function createQuestion(req, res, next) {
+async function examSchema(req, res, next) {
+  const schema = Joi.object({
+    title: Joi.string().required(),
+    startTime: Joi.date().timestamp().required(),
+    endTime: Joi.date().timestamp().required(),
+  });
+
+  try {
+    validateHelpers.validateRequest(req, res, next, schema, 'body');
+  } catch (err) {
+    console.log('login schema', err);
+    commonHelpers.errorHandler(req, res, err.message, err.statusCode);
+  }
+}
+
+async function createQuestionSchema(req, res, next) {
   const schema = Joi.object({
     question: Joi.string().min(3).required(),
     type: Joi.string().valid('single_choice', 'multi_choice').required(),
@@ -24,4 +39,17 @@ async function createQuestion(req, res, next) {
   }
 }
 
-module.exports = { createQuestion };
+async function addUserSchema(req, res, next) {
+  const schema = Joi.object({
+    userId: Joi.string().required(),
+  });
+
+  try {
+    validateHelpers.validateRequest(req, res, next, schema, 'body');
+  } catch (err) {
+    console.log('login schema', err);
+    commonHelpers.errorHandler(req, res, err.message, err.statusCode);
+  }
+}
+
+module.exports = { examSchema, createQuestionSchema, addUserSchema };
