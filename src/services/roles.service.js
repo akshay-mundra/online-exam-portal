7a1c8c477efc06ae68d733a21fe0b2b2dbd25847
Akshay Mundra <akshay@gkmit.co>
2024-11-16 14:23:40 +0530

    fix: [oep-29] update roles services

--- src/services/roles.service.js
+++ src/services/roles.service.js
@@ -1,15 +1,26 @@
 const { Role } = require('../models');
 const commonHelpers = require('../helpers/common.helper');
+const { sequelize } = require('../models');
 
 async function create(payload) {
   const { name } = payload;
-  const role = await Role.findOne({ where: { name } });
-  if (role) {
-    commonHelpers.throwCustomError('Role already exist', 400);
+  const transactionContext = await sequelize.transaction();
+
+  try {
+    const role = await Role.findOne({ where: { name } });
+    if (role) {
+      commonHelpers.throwCustomError('Role already exist', 400);
+    }
+    const newRole = await Role.create(
+      { name },
+      { transaction: transactionContext },
+    );
+
+    return newRole;
+  } catch (err) {
+    await transactionContext.rollback();
+    throw err;
   }
-  const newRole = await Role.create({ name });
-  console.log(newRole);
-  return newRole;
 }
 
 async function get(id) {
@@ -27,19 +38,41 @@ async function getAll() {
 
 async function update(id, payload) {
   const { name } = payload;
-  const role = await Role.findOne({ where: { id } });
-  role.name = name;
-  await role.save();
-  return role;
+  const transactionContext = await sequelize.transaction();
+
+  try {
+    const role = await Role.findOne({ where: { id } });
+    role.name = name;
+    await role.save({ transaction: transactionContext });
+
+    transactionContext.commit();
+
+    return role;
+  } catch (err) {
+    await transactionContext.rollback();
+    throw err;
+  }
 }
 
 async function remove(id) {
-  const role = await Role.findOne({ where: { id } });
-  if (!role) {
-    commonHelpers.throwCustomError('Role not found', 404);
+  const transactionContext = await sequelize.transaction();
+
+  try {
+    const role = await Role.findOne({ where: { id } });
+
+    if (!role) {
+      commonHelpers.throwCustomError('Role not found', 404);
+    }
+    await Role.destroy(
+      { where: { id: role.id } },
+      { transaction: transactionContext },
+    );
+
+    return 'Role removed successfully';
+  } catch (err) {
+    await transactionContext.rollback();
+    throw err;
   }
-  await Role.destroy({ where: { id: role.id } });
-  return 'Role removed successfully';
 }
 
 module.exports = { create, get, getAll, update, remove };
