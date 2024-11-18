c117bece6686473029274f98a984909420bdc218
Akshay Mundra <akshay@gkmit.co>
2024-11-18 16:29:22 +0530

    feat: [oep-36] udpate eslint and prettierc for test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,23 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import pluginJest from 'eslint-plugin-jest';
 
 export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  {
+    files: ['**/*.js'],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+    },
+  },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
 ];
