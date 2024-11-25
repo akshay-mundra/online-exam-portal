8a290a0cc578b11ad3127eb5f61d45764f036c11
Akshay Mundra <akshay@gkmit.co>
2024-11-25 18:50:47 +0530

    fix: [oep-48] remove old dead code from jwt helper test

--- tests/helpers/jwt.helper.test.js
+++ tests/helpers/jwt.helper.test.js
@@ -6,7 +6,7 @@ jest.mock('jose');
 describe('JWT Helper Functions', () => {
   const mockDecodedSecret = Buffer.from([
     154, 135, 36, 250, 199, 156, 173, 235,
-  ]); // Mock decoded secret
+  ]);
   const mockPayload = { id: 1, role: 'admin' };
   const mockOptions = { expiresIn: '1h' };
   const mockToken = 'mock-jwt-token';
@@ -15,7 +15,6 @@ describe('JWT Helper Functions', () => {
     process.env.JWT_SECRET = 'mock-secret';
     process.env.JWT_EXPIRE_IN = '1h';
 
-    // Mock jose.base64url.decode to return the mockDecodedSecret
     jose.base64url = {
       decode: jest.fn(() => mockDecodedSecret),
     };
@@ -27,7 +26,6 @@ describe('JWT Helper Functions', () => {
 
   describe('signToken', () => {
     it('should generate a JWT token with valid payload and options', async () => {
-      // Mock EncryptJWT behavior
       const mockEncryptJWT = {
         setProtectedHeader: jest.fn().mockReturnThis(),
         setIssuedAt: jest.fn().mockReturnThis(),
@@ -80,69 +78,3 @@ describe('JWT Helper Functions', () => {
     });
   });
 });
-
-// const jwt = require('jsonwebtoken');
-// const { signToken, verifyToken } = require('../../src/helpers/jwt.helper');
-
-// //
-// jest.mock('jsonwebtoken');
-
-// describe('JWT Helper Functions', () => {
-//   describe('signToken', () => {
-//     it('should sign a JWT token with the given payload and options', () => {
-//       const payload = { userId: 123 };
-//       const options = { expiresIn: '1h' };
-//       const secret = process.env.JWT_SECRET;
-//       const token = 'mocked-jwt-token';
-
-//       jwt.sign.mockReturnValue(token);
-
-//       const result = signToken(payload, options);
-
-//       expect(jwt.sign).toHaveBeenCalledWith(payload, secret, options);
-//       expect(result).toBe(token);
-//     });
-
-//     it('should use default expiration option when not provided', () => {
-//       const payload = { userId: 123 };
-//       const defaultOptions = { expiresIn: process.env.JWT_EXPIRE_IN };
-//       const token = 'mocked-jwt-token';
-
-//       jwt.sign.mockReturnValue(token);
-
-//       const result = signToken(payload);
-
-//       expect(jwt.sign).toHaveBeenCalledWith(
-//         payload,
-//         process.env.JWT_SECRET,
-//         defaultOptions,
-//       );
-//       expect(result).toBe(token);
-//     });
-//   });
-
-//   describe('verifyToken', () => {
-//     it('should verify the JWT token and return the decoded payload', () => {
-//       const token = 'mocked-jwt-token';
-//       const decodedPayload = { userId: 123 };
-
-//       jwt.verify.mockReturnValue(decodedPayload);
-
-//       const result = verifyToken(token);
-
-//       expect(jwt.verify).toHaveBeenCalledWith(token, process.env.JWT_SECRET);
-//       expect(result).toEqual(decodedPayload);
-//     });
-
-//     it('should throw an error if the token is invalid', () => {
-//       const token = 'invalid-token';
-//       const error = new Error('invalid token');
-
-//       jwt.verify.mockImplementation(() => {
-//         throw error;
-//       });
-
-//       expect(() => verifyToken(token)).toThrow(error);
-//     });
-//   });
-// });
