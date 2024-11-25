d849e1d999c0e2aa081e5ab94ed03cbdb2f0ee86
Akshay Mundra <akshay@gkmit.co>
2024-11-25 17:19:46 +0530

    feat: [oep-46] add jwt encryption using jose package

--- src/helpers/jwt.helper.js
+++ src/helpers/jwt.helper.js
@@ -1,15 +1,22 @@
-const jwt = require('jsonwebtoken');
+const jose = require('jose');
 
-// sign jwt token
-function signToken(
+const secret = jose.base64url.decode(process.env.JWT_SECRET);
+
+async function signToken(
   payload,
   options = { expiresIn: process.env.JWT_EXPIRE_IN },
 ) {
-  return jwt.sign(payload, process.env.JWT_SECRET, options);
+  const jwt = await new jose.EncryptJWT(payload)
+    .setProtectedHeader({ alg: 'dir', enc: 'A128CBC-HS256' })
+    .setIssuedAt()
+    .setExpirationTime(options.expiresIn)
+    .encrypt(secret);
+  return jwt;
 }
 
-// verify the jwt token
-function verifyToken(token) {
-  return jwt.verify(token, process.env.JWT_SECRET);
+async function verifyToken(token) {
+  const { payload } = await jose.jwtDecrypt(token, secret);
+  return payload;
 }
+
 module.exports = { signToken, verifyToken };
