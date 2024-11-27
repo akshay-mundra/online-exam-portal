e128966285b10cad54dc29f10eb294d0e5f722c1
Akshay Mundra <akshay@gkmit.co>
2024-11-27 17:45:52 +0530

    fix: [oep-61] update question and option model column type

--- src/models/Question.js
+++ src/models/Question.js
@@ -39,7 +39,7 @@ module.exports = (sequelize, DataTypes) => {
         }
       },
       question: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false
       },
       type: {
