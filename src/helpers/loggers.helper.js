f357104e05fdc78b29e4067cde63451e84cb1451
Akshay Mundra <akshay@gkmit.co>
2024-11-27 23:04:28 +0530

    feat: [oep-66] add silent true on test in loggers

--- src/helpers/loggers.helper.js
+++ src/helpers/loggers.helper.js
@@ -4,7 +4,8 @@ const { combine, timestamp, json, errors } = winston.format;
 const logger = winston.createLogger({
   level: 'info',
   format: combine(errors({ stack: true }), timestamp(), json()),
-  transports: [new winston.transports.Console()]
+  transports: [new winston.transports.Console()],
+  silent: process.env.NODE_ENV === 'test'
 });
 
 module.exports = { logger };
